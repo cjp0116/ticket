af84e958480bcd4375d04c9cbe85359d8718ac15
Jae Pil Cho <48457791+cjp0116@users.noreply.github.com>
2021-02-10 14:34:25 -0800

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require("pg");
-const { DB_URI } = require('./config');
-
-const client = new Client({
-  connectionString : DB_URI
-});
-
-client.connect();
-module.exports = client;
