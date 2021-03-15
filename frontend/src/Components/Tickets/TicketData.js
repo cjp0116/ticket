621455f09aa4f64cf9143260d3317e75aa624221
Jae Cho <jc960116@gmail.com>
2021-03-14 17:22:13 -0700

    edit

--- frontend/src/Components/Tickets/TicketData.js
+++ frontend/src/Components/Tickets/TicketData.js
@@ -51,13 +51,13 @@ const TicketDetails = (props) => {
         </Link>
       </Table.Cell>
       <Table.Cell>
-        {/* <Link to={`tickets/${t.id}/edit`} style={{ textDecoration: "none" }}>
+        <Link to={`tickets/${t.id}/edit`} style={{ textDecoration: "none" }}>
           <Popup
             content={`Edit ticket ${t.id}`}
             trigger={<Button icon="edit" size="small" color="green" circular />}
           />
-        </Link> */}
-        {/* <Popup
+        </Link>
+        <Popup
           content={`Edit ticket ${t.id}`}
           trigger={
             <Button 
@@ -68,8 +68,8 @@ const TicketDetails = (props) => {
               onClick={() => setShowEdit(true)}
             />
           }
-        /> */}
-        {/* <Confirm 
+        />
+        <Confirm 
           open={showEdit}
           header={`Edit ticket ${t.id}`}
           content={
@@ -100,7 +100,7 @@ const TicketDetails = (props) => {
           confirmButton="Delete"
           onCancel={() => props.setOpenConfirm(false)}
           onConfirm={() => props.handleDeleteConfirm(t.id)}
-        /> */}
+        />
       </Table.Cell>
     </Table.Row>
   ));
--- frontend/src/Routes/Routes.js
+++ frontend/src/Routes/Routes.js
@@ -10,24 +10,31 @@ import NewUserForm from "../Components/Users/NewUserForm";
 const Routes = props => {
   return (
     <Switch>
+      
       <Route exact path="/newTicket">
         <NewTicketForm />
       </Route>
+      
       <Route exact path="/login">
         <Login />
       </Route>
+      
       <Route exact path="/tickets">
         <TicketList mine />
       </Route>
+      
       <Route exact path="/group">
         <TicketList group />
       </Route>
+      
       <Route exact path="/recent">
         <TicketList recent />
       </Route>
+      
       <Route exact path="/search">
         <SearchTicketPage />
       </Route>
+
       <Route exact path="/newUser">
         <NewUserForm />
       </Route>
